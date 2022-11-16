97d96f01bb464cd3b32e746e3029ab514d74dce3
nodi37 <nodi37@o2.pl>
2022-11-16 16:15:19 +0100

    Small request fix in image miniature

--- frontend/src/components/ComplexComponents/ImageMiniature.vue
+++ frontend/src/components/ComplexComponents/ImageMiniature.vue
@@ -14,20 +14,25 @@ export default {
 		},
 	},
 	mounted: async function () {
-		if (!this.imgId) {
-			this.imageData = this.imgSrc;
-		} else {
-			//Can add size prop to search
-			//This is smallest size so every bigger size will fit too
-			const loadedImg = this.imgsArr.find((img) => img._id === this.imgId);
-
-			if (!loadedImg) {
-				const imgDoc = await this.getImageById(this.imgId, "mini");
-				store.dispatch("PUSH_NEW_IMAGE", { ...imgDoc, size: "mini" });
-				this.imageData = imgDoc.data;
+		try {
+			if (!this.imgId) {
+				this.imageData = this.imgSrc;
 			} else {
-				this.imageData = loadedImg.data;
+				//Can add size prop to search
+				//This is smallest size so every bigger size will fit too
+				const loadedImg = this.imgsArr.find((img) => img._id === this.imgId);
+
+				if (!loadedImg) {
+					const imgDoc = await this.getImageById(this.imgId, "mini");
+					store.dispatch("PUSH_NEW_IMAGE", { ...imgDoc, size: "mini" });
+					this.imageData = imgDoc.data;
+				} else {
+					this.imageData = loadedImg.data;
+				}
 			}
+		} catch (error) {
+			this.imageData = null;
+			store.dispatch("PUSH_NEW_IMAGE", { _id: this.imgId, data: null, size: "mini" });
 		}
 	},
 	mixins: [imageController],
@@ -35,7 +40,7 @@ export default {
 </script>
 
 <template>
-	<figure class="flex-none w-32 h-32 rounded overflow-hidden drop-shadow-lg relative group">
+	<figure v-if="imageData" class="flex-none w-32 h-32 rounded overflow-hidden drop-shadow-lg relative group">
 		<div
 			class="absolute h-full w-full flex flex-col gap-1 justify-center align-center opacity-0 group-hover:opacity-100 transition"
 		>
